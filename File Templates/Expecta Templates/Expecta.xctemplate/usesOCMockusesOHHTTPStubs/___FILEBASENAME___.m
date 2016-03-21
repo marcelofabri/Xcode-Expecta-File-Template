53b9cb3698063e65967c2d8443136f78c1061c7d
Marcelo Fabri <marcelofabrimf@gmail.com>
2016-03-20 23:15:28 -0300

    Initial commit

---
+++ File Templates/Expecta Templates/Expecta.xctemplate/usesOCMockusesOHHTTPStubs/___FILEBASENAME___.m
@@ -0,0 +1,25 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Expecta/Expecta.h>
+#import <OCMock/OCMock.h>
+#import <OHHTTPStubs/OHHTTPStubs.h>
+#import <OHHTTPStubs/OHPathHelpers.h>
+#import <XCTest/XCTest.h>
+
+#import "___VARIABLE_classToTest:identifier___.h"
+
+@interface ___VARIABLE_classToTest:identifier___Tests : XCTestCase
+
+@end
+
+@implementation ___VARIABLE_classToTest:identifier___Tests
+
+@end
+
+
---
+++ File Templates/Expecta Templates/Expecta.xctemplate/usesOHHTTPStubs/___FILEBASENAME___.m
@@ -0,0 +1,24 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Expecta/Expecta.h>
+#import <OHHTTPStubs/OHHTTPStubs.h>
+#import <OHHTTPStubs/OHPathHelpers.h>
+#import <XCTest/XCTest.h>
+
+#import "___VARIABLE_classToTest:identifier___.h"
+
+@interface ___VARIABLE_classToTest:identifier___Tests : XCTestCase
+
+@end
+
+@implementation ___VARIABLE_classToTest:identifier___Tests
+
+@end
+
+
